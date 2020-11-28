e4c9f47a23bc31e929b41fa7ceff0fc328c67699
Ivan Garcia <isc.ivgarcia@gmail.com>
2020-11-28 13:26:58 -0600

    Practica 6 - Spring Data REST

--- 6-Spring-Data-REST/src/main/java/mx/org/certificatic/springboot/practica6/repositories/UserRepository.java
+++ 6-Spring-Data-REST/src/main/java/mx/org/certificatic/springboot/practica6/repositories/UserRepository.java
@@ -5,16 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import mx.org.certificatic.springboot.practica6.entities.User;
 
 // define rest repository
 @CrossOrigin
-@RepositoryRestResource(path = "usuarios",
-						collectionResourceRel = "amigos",
-						itemResourceRel = "my_self")
+@RepositoryRestResource(path = "usuarios", collectionResourceRel = "amigos", itemResourceRel = "my_self")
 public interface UserRepository extends JpaRepository<User, Long> { // Implementa JpaRepository
 
 	// define rest resource
