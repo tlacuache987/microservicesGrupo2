f83f5e5d8b356b055886f7b75a0fcd8169988749
Ivan Garcia <isc.ivgarcia@gmail.com>
2021-01-23 11:48:33 -0600

    Practica 17 - Spring Boot Admin

--- 17-Spring-Boot-Admin-Client/src/main/java/mx/org/certificatic/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
+++ 17-Spring-Boot-Admin-Client/src/main/java/mx/org/certificatic/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
@@ -1,8 +1,23 @@
 package mx.org.certificatic.springboot.practica17.springbootadminclient.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RestController
 public class SpringBootAdminClientController {
 
+	@Value("${spring.application.name}")
+	private String appName;
+
+	@GetMapping
+	public String hello() {
+		log.info("Sending hello !!!");
+		
+		return "Hello I'm the "+appName+" app.";
+	}
+	
 }
--- 17-Spring-Boot-Admin-Server/src/main/java/mx/org/certificatic/springboot/practica17/springbootadmin/Application.java
+++ 17-Spring-Boot-Admin-Server/src/main/java/mx/org/certificatic/springboot/practica17/springbootadmin/Application.java
@@ -3,9 +3,12 @@ package mx.org.certificatic.springboot.practica17.springbootadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 
 @SpringBootApplication
 // Enable Spring Boot Admin Server
+@EnableAdminServer
 public class Application {
 
 	public static void main(String[] args) {
