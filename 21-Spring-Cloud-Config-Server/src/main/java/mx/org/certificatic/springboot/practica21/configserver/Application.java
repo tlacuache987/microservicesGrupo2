3afc86cd9631d6732462004a45d40ad46c4c10cf
Ivan Garcia <isc.ivgarcia@gmail.com>
2021-02-06 16:12:14 -0600

    Practica 21 Parte 1 - Spring Cloud Config Server

--- 21-Spring-Cloud-Config-Server/src/main/java/mx/org/certificatic/springboot/practica21/configserver/Application.java
+++ 21-Spring-Cloud-Config-Server/src/main/java/mx/org/certificatic/springboot/practica21/configserver/Application.java
@@ -2,8 +2,10 @@ package mx.org.certificatic.springboot.practica21.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 // Habilita Config Server
+@EnableConfigServer
 @SpringBootApplication
 public class Application {
 
