c16fedf845036c84b80016d9fbcf501cc59f7145
Ivan Garcia <isc.ivgarcia@gmail.com>
2021-02-13 11:35:49 -0600

    Practica 21 Parte 1 - Spring Cloud Config Server (actualizacion a yml y spring cloud bus- Hello Service)

--- 21-Hello-Spring-Cloud-Config-Client/src/main/java/mx/org/certificatic/springboot/practica21/hello/configclient/Application.java
+++ 21-Hello-Spring-Cloud-Config-Client/src/main/java/mx/org/certificatic/springboot/practica21/hello/configclient/Application.java
@@ -1,11 +1,15 @@
 package mx.org.certificatic.springboot.practica21.hello.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class Application {
@@ -23,5 +27,28 @@ public class Application {
 	@GetMapping
 	public String hi() {
 		return hi + ", using profile: " + profile;
+	}
+
+	@Value("${someKey:no value}")
+	private String someKey;
+
+	@GetMapping("/key")
+	public String someKey() {
+		return "someKey = " + someKey;
+	}
+	
+	@Value("${guest}")
+	private String guest;
+	
+	@Bean
+	public CommandLineRunner startup() {
+		return (args) -> {
+			System.out.println("guest: " + guest);
+		};
+	}
+	
+	@GetMapping("/guest")
+	public String guest() {
+		return "guest = " + guest;
 	}
 }
--- 21-Users-Spring-Cloud-Config-Client/src/main/java/mx/org/certificatic/springboot/practica21/users/configclient/Application.java
+++ 21-Users-Spring-Cloud-Config-Client/src/main/java/mx/org/certificatic/springboot/practica21/users/configclient/Application.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class Application {
@@ -33,5 +35,13 @@ public class Application {
 			System.out.println(hi);
 			System.out.println(profile);
 		};
+	}
+	
+	@Value("${someKey:no value}")
+	private String someKey;
+
+	@GetMapping("/key")
+	public String someKey() {
+		return "someKey = " + someKey;
 	}
 }
